3d1eec938406e9ae2e867bce5ee05fc7aa17fea7
Samanvith M S <samanvith2002@gmail.com>
2020-09-11 14:50:47 +0530

    updated with completely working progressbar

--- timeline.js
+++ timeline.js
@@ -1,29 +1,4 @@
-AOS.init();
 
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  // startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  startEvent: 'load',
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-});
 
 
 var btn1=document.getElementById("btn1");
@@ -31,14 +6,39 @@ var btn2=document.getElementById("btn2");
 var btn3=document.getElementById("btn3");
 var day1=document.getElementsByClassName("day1");
 var day2=document.getElementsByClassName("day2");
+const progressbar = document.querySelector('progress')
+const article = document.querySelector('.complete')
+const progressbar2=document.getElementById('progressbar2')
+const article2=document.getElementById('d2')
+let isScrolling = false
+let isScrolling2=false
+
+document.addEventListener('scroll', (e) => isScrolling = true)
+
+render() 
+
+function render() {
+	
+	requestAnimationFrame(render)
+	
+	if (!isScrolling) return
+	
+	progressbar.value = (window.scrollY )/ (article.offsetHeight - window.innerHeight) * 100
+	progressbar2.value=window.scrollY/(article2.offsetHeight-window.innerHeight)*100
+  isScrolling = false
+  console.log(render.call)
+	
+}
+
 btn1.addEventListener("click",function(){
-    for (var i=0;i<day2.length;i+=1){
+  
+  for (var i=0;i<day2.length;i+=1){
         day2[i].style.display = 'none';
        
       }
       for (var i=0;i<day1.length;i+=1){
         day1[i].style.display = 'block';
-        
+       
       }
       AOS.refresh();
 }
@@ -48,6 +48,7 @@ btn2.addEventListener("click",function(){
   
   for (var i=0;i<day2.length;i+=1){
     day2[i].style.display = 'block';
+   
     
   }
   for (var i=0;i<day1.length;i+=1){
@@ -75,55 +76,31 @@ btn3.addEventListener("click",function(){
   $(".btn1").click(function () {
     $(".timeline:before .active").toggle();
 });
-//   //let element = document.querySelector('.timeline::before')
-//   //let ab = window.getComputedStyle(element, '::before');
-//   //$(window).scroll(function() {
-//     var windowpos = $(window).scrollTop();
-//    // $('.timeline:before').toggleClass('.timeline:before.active');
-//     // $('.progress-bar--circle').removeClass('active');
 
-//     if (windowpos > $('.top').offset().top) {
-//       //$('.timeline ul li button').removeClass('active');
-//       $('.timeline:before').toggleClass('.timeline:before .active');
-     
-//       //let style = window.getComputedStyle(element, '::before').setProperty("background-color","#305bc9");
-//     } //windowpos
 
-//     if (windowpos > $('#top1').offset().top) {
-//       // $('.timeline ul li button').removeClass('active');
-//      // element.style.backgroundColor="#305bc9";
-//       //$('.timeline').toggleClass('.active');
-//       // $('a[href$="#article-1"] .progress-bar--circle').addClass('active');
-//     }
-//     var scrollTop = $(window).scrollTop(),
-//     // document height
-//     docHeight = $(document).height(),
-//     // window height
-//     winHeight = $(window).height(),
-//     // percent of document scrolled
-//     scrollPercent = (scrollTop) / (docHeight - winHeight),
-//     scrollPercentRounded = Math.round(scrollPercent*100);
+AOS.init();
 
-// // incement progress bar as user scrolls
-// $('.timeline:before').css('height', scrollPercentRounded + '%');
-//   });
-const progressbar = document.querySelector('progress')
-const article = document.querySelector('.complete')
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  // startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  startEvent: 'load',
 
-let isScrolling = false
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
-document.addEventListener('scroll', (e) => isScrolling = true)
-
-render()
-
-function render() {
-	
-	requestAnimationFrame(render)
-	
-	if (!isScrolling) return
-	
-	progressbar.value = (window.scrollY )/ (article.offsetHeight - window.innerHeight) * 100
-	
-	isScrolling = false
-	
-}
+});
